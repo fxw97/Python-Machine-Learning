b3e1924795a7f06fb57c6868cf89516bdbcdd67d
xuewei <1550505935@qq.com>
2021-08-25 20:48:39 +0800

    commit

---
+++ 第3章 线性回归/3.3 模型保存.py
@@ -0,0 +1,28 @@
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import joblib
+
+boston = load_boston()
+x_train, x_test, y_train, y_test = train_test_split(boston.data,boston.target,test_size=0.2,random_state=22)
+
+transfer = StandardScaler()
+transfer.fit_transform(x_train)
+transfer.fit_transform(x_test)
+
+estimator = LinearRegression()
+estimator.fit(x_train, y_train)
+
+# 保存模型
+joblib.dump(estimator,'3.3 linear model.pkl')
+
+print('这个模型的偏置是：\n',estimator.intercept_)
+print('这个模型的系数是：\n',estimator.coef_)
+
+y_pred = estimator.predict(x_test)
+print('预测值是：\n',y_pred)
+
+ret = mean_squared_error(y_test,y_pred)
+print('均方误差:\n',ret)
